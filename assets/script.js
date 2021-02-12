7a22abf80115a1344fa48b00c99ad32e4588b198
clara_r <crenauro@optimum.net>
2021-02-11 20:07:14 -0500

    updated html to fix errors, updated js file

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-$(document).ready(funtion() {
+$(document).ready(funtion());{
 
 //check for button clicks inside an on click
-$(".saveBtn").on)("click", function() {
+$(".saveBtn").on("click", function() {
 //grab values for time and text
 var value = $(this).siblings(".task").val();
 var time = $(this).parent().attr("id")
@@ -11,20 +11,44 @@ localStorage.setItem(time, value)
 
 //write a function to update the hours in the time blocks
 //get current time using moment
-var currentDay = moment().hours();
+var currentTime = moment().hours();
+var workDay = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+};
+
+$(document).ready(function(){
+    if(!localStorage.getItem('workDay')) {
+        updateCalendarTasks(workDay);
+    } else {
+        updateCalendarTasks(JSON.parse(localStorage.getItem('workday')));
+    }
+})
 
 //then you need to find a way to loop all your hour blocks get from every element the hour and compare it with the currentHour
 
-function currentHour {
-    if (now == hours).document.querySelector("col-1 hour")
-    change col-1 hour to color: #green;
-}   else change col-1 hour to color: #red;
+if (currentTime < presentHour) {
+    $(textEntry).addClass("past-hour");
+} else if (currentTime > presentHour) {
+    $(textEntry).addClass("future-hour");
+} else {
+    $(textEntry).addClass("present-hour");
+}
 
 
 //check to see if we have moved past the current time
 //if
 //addClass
 //addClass ("past")
+
 
 //check to see if current time
 //else if
@@ -46,8 +70,33 @@ $("#hour-9 .task").val(localStorage.getItem("hour-9"));
 
 TODO:  //this doesn't work
 
-// var today = moment();
-$("currentDay").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+var today = moment();
+console.log("today: ", today);
+console.log("moment().format('dddd'): ", moment().format('dddd'));
+console.log(moment);
+$("#dateToday").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-// var today = moment();
-// $("#1a").text(today.format("MMM Do, YYYY"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

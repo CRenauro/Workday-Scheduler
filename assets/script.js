ad9ac559735cd8c647f1deee1df6eb92f23f8c48
clara_r <crenauro@optimum.net>
2021-02-13 17:34:08 -0500

    updated js and html file

--- assets/script.js
+++ assets/script.js
@@ -1,102 +1,73 @@
-$(document).ready(funtion());{
+//$(document).ready(function(){
 
 //check for button clicks inside an on click
-$(".saveBtn").on("click", function() {
-//grab values for time and text
-var value = $(this).siblings(".task").val();
-var time = $(this).parent().attr("id")
-//save to local storage
-localStorage.setItem(time, value)
+$(".saveBtn").on("click", function(){
+    //grab values for time and text
+    var value = $(this).siblings(".textInput").val()
+    var time = $(this).parent().text()
+    console.log(value, time)
+    //save to local storage
+    localStorage.setItem(time, value)
+}) 
+
+var container = $(".container")
+container.children().each(function() {
+var element = $(this).prop('outerHTML');
+$("col-1 hour").append(element);
 })
 
-//write a function to update the hours in the time blocks
-//get current time using moment
-var currentTime = moment().hours();
-var workDay = {
-    "8 AM": "",
-    "9 AM": "",
-    "10 AM": "",
-    "11 AM": "",
-    "12 PM": "",
-    "1 PM": "",
-    "2 PM": "",
-    "3 PM": "",
-    "4 PM": "",
-    "5 PM": "",
-};
 
-$(document).ready(function(){
-    if(!localStorage.getItem('workDay')) {
-        updateCalendarTasks(workDay);
-    } else {
-        updateCalendarTasks(JSON.parse(localStorage.getItem('workday')));
-    }
-})
+var today = moment();
+$("#dateToday").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm'));
 
-//then you need to find a way to loop all your hour blocks get from every element the hour and compare it with the currentHour
+var presentHour = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17" ];
 
-if (currentTime < presentHour) {
+
+// //then you need to find a way to loop all your hour blocks get from every element the hour and compare it with the currentHour
+// var presentHour = moment().hours();
+
+
+if (presentHour < moment) {
     $(textEntry).addClass("past-hour");
-} else if (currentTime > presentHour) {
+} else if (presentHour > moment) {
     $(textEntry).addClass("future-hour");
 } else {
     $(textEntry).addClass("present-hour");
 }
 
-
-//check to see if we have moved past the current time
-//if
-//addClass
-//addClass ("past")
-
-
-//check to see if current time
-//else if
-//removeClass
-//addClass
-
-//check to see if time is future
-//else
-//remove class
-//remove class
-//add class
-}
-
-//load any saved data from localStorage
-
-$("#hour-9 .task").val(localStorage.getItem("hour-9"));
-
-//display current day on page (use moment)
-
-TODO:  //this doesn't work
-
-var today = moment();
-console.log("today: ", today);
-console.log("moment().format('dddd'): ", moment().format('dddd'));
-console.log(moment);
-$("#dateToday").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+// //check to see if we have moved past the current time
+// //if
+// //addClass
+// //addClass ("past")
+// if (pastTime > currentTime) {
+//     $(textEntry).addClass ("past-hour");
+// }
 
 
+// //check to see if current time
+// // for loop
+// //else if
+// //removeClass
+// //addClass
+// if (currentTime == currentTime) {
+//     $(textEntry).removeClass ("past-hour")
+// } else {
+//     $(textEntry).addClass ("present-hour");
+
+// //check to see if time is future
+// //else
+// //remove class
+// //remove class
+// //add class
+// if (currentTime < futureHour) {
+//     $(textEntry).removeClass ("past-hour") 
+// }   else if {
+//     $(textEntry).removeClass ("present-hour")
+// }   else {
+//     $(textEntry).addClass ("future-hour")
+// };   
 
 
+// //load any saved data from localStorage
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// //});

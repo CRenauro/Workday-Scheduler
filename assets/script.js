ffd382980c97740633a5a72d76c6a57576e5bca5
CRenauro <crenauro@optimum.net>
2021-05-12 09:27:43 -0400

    changed fonts, added padding, added color to header

--- assets/script.js
+++ assets/script.js
@@ -35,6 +35,15 @@ $(document).ready(function () {
         })
     }
 
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     timeTracker();
 
